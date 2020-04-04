52bc422f217089229afca8ae2147045466cd67f9
techlist <github@techlist.com>
2020-04-03 17:22:42 -0700

    Iterate over a daterange instead of handling 1 date.

--- calendar/make_calendar.py
+++ calendar/make_calendar.py
@@ -1,6 +1,10 @@
 import datetime
 
-d = datetime.date(2020, 4, 2)
-print("date is {date}".format(date=d))
-print("Day of week is {dow}.".format(dow=d.weekday()))
+startdate = datetime.date(2019, 8, 1)
+enddate = datetime.date(2020, 4, 30)
 
+d = startdate
+while d <= enddate:
+    print("date is {date}".format(date=d))
+    print("Day of week is {dow}.".format(dow=d.weekday()))
+    d = d + datetime.timedelta(1)
