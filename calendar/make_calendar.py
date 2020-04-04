8da737d6cd2908934a77a41b45ed85d9ba06ac93
techlist <github@techlist.com>
2020-04-03 18:26:16 -0700

    Move all the calendar code into main function.

--- calendar/make_calendar.py
+++ calendar/make_calendar.py
@@ -1,27 +1,31 @@
 import datetime
 
-startdate = datetime.date(2019, 8, 1)
-enddate = datetime.date(2020, 4, 30)
+def main():
+    startdate = datetime.date(2019, 8, 1)
+    enddate = datetime.date(2020, 4, 30)
 
-names_of_days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",   
-]
+    names_of_days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",   
+    ]
 
-d = startdate
-while d <= enddate:
-    dow = d.weekday()
-    dow_name = names_of_days[dow]
+    d = startdate
+    while d <= enddate:
+        dow = d.weekday()
+        dow_name = names_of_days[dow]
 
-    if dow_name == "Sunday":
-        print()
-        print('Time for another week')  
+        if dow_name == "Sunday":
+            print()
+            print('Time for another week')  
 
-    print("date is {date}".format(date=d))
-    print("Day of week is {dow}.".format(dow=dow_name))
-    d = d + datetime.timedelta(1)
+        print("date is {date}".format(date=d))
+        print("Day of week is {dow}.".format(dow=dow_name))
+        d = d + datetime.timedelta(1)
+
+if __name__ == "__main__":
+    main()
