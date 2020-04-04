f45060f4027ce5de4ec918beea5dc21c1cef9609
techlist <github@techlist.com>
2020-04-03 18:05:57 -0700

    display the day of the week and group together the weeks

--- calendar/make_calendar.py
+++ calendar/make_calendar.py
@@ -3,8 +3,25 @@ import datetime
 startdate = datetime.date(2019, 8, 1)
 enddate = datetime.date(2020, 4, 30)
 
+names_of_days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",   
+]
+
 d = startdate
 while d <= enddate:
+    dow = d.weekday()
+    dow_name = names_of_days[dow]
+
+    if dow_name == "Sunday":
+        print()
+        print('Time for another week')  
+
     print("date is {date}".format(date=d))
-    print("Day of week is {dow}.".format(dow=d.weekday()))
+    print("Day of week is {dow}.".format(dow=dow_name))
     d = d + datetime.timedelta(1)
