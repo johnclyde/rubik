7b7cbc1761dcdf0393eff616f596cd8f2a3f50fe
techlist <github@techlist.com>
2020-04-03 19:41:43 -0700

    Move reder_week inside a DumbRenderer object.

--- calendar/make_calendar.py
+++ calendar/make_calendar.py
@@ -1,5 +1,5 @@
 import datetime
-
+from .dumb_renderer import DumbRenderer
 
 names_of_days = [
     "Monday",
@@ -12,20 +12,12 @@ names_of_days = [
 ]
 
 
+def main(
+    renderer,
+    startdate,
+    enddate,
+):
 
-def render_week(week):
-    print()
-    print('time for another week')
-    for d in week:
-        dow = d.weekday()
-        dow_name = names_of_days[dow]
-        print("date is {date}".format(date=d))
-        print("Day of week is {dow}.".format(dow=dow_name))
-
-
-def main():
-    startdate = datetime.date(2019, 8, 1)
-    enddate = datetime.date(2020, 4, 30)
     week = []
 
     d = startdate
@@ -34,13 +26,22 @@ def main():
         dow_name = names_of_days[dow]
 
         if dow_name == "Sunday":
-            render_week(week)
+            renderer.render_week(week)
             week = []
 
         week.append(d) 
         d = d + datetime.timedelta(1)
 
-    render_week(week)
+    renderer.render_week(week)
+
 
 if __name__ == "__main__":
-    main()
+    renderer = DumbRenderer()
+    startdate = datetime.date(2019, 8, 1)
+    enddate = datetime.date(2020, 4, 30)
+
+    main(
+        renderer=renderer,
+        startdate=startdate,
+        enddate=enddate, 
+    )
