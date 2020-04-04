7c5dd0768e20f75985e153c60812d3fd4a5c235f
techlist <github@techlist.com>
2020-04-03 19:02:03 -0700

    Process weeks in batches

--- calendar/make_calendar.py
+++ calendar/make_calendar.py
@@ -1,18 +1,32 @@
 import datetime
 
+
+names_of_days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",     
+]
+
+
+
+def render_week(week):
+    print()
+    print('time for another week')
+    for d in week:
+        dow = d.weekday()
+        dow_name = names_of_days[dow]
+        print("date is {date}".format(date=d))
+        print("Day of week is {dow}.".format(dow=dow_name))
+
+
 def main():
     startdate = datetime.date(2019, 8, 1)
     enddate = datetime.date(2020, 4, 30)
-
-    names_of_days = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",   
-    ]
+    week = []
 
     d = startdate
     while d <= enddate:
@@ -20,12 +34,13 @@ def main():
         dow_name = names_of_days[dow]
 
         if dow_name == "Sunday":
-            print()
-            print('Time for another week')  
+            render_week(week)
+            week = []
 
-        print("date is {date}".format(date=d))
-        print("Day of week is {dow}.".format(dow=dow_name))
+        week.append(d) 
         d = d + datetime.timedelta(1)
+
+    render_week(week)
 
 if __name__ == "__main__":
     main()
